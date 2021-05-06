6069edbd6c470da1587683757662e68b544de7a1
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-05-06 20:09:48 +0600

    completed globalVariable.js

--- globalVariable.js
+++ globalVariable.js
@@ -1,15 +1,12 @@
 
 //__dirname - path to current directory
-
 const currentDirectory = __dirname;
 console.log(currentDirectory);
 
 //__filename = filename
-
 console.log(__filename);
 
 //require - function to provide modules
-
 const fs = require('fs');
 
 //module - info about current module
